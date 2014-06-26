9627f17240bfe6508ae953e65faa50959073bd59
Martin Repta <mrepta@gratex.com>
2014-06-26 19:02:03 +0200

    [FIX] column hider now supports nested properties binding

--- extensions/ColumnHider.js
+++ extensions/ColumnHider.js
@@ -1,5 +1,5 @@
-define(["dojo/_base/declare", "dojo/has", "dojo/on", "../util/misc", "put-selector/put", "dojo/i18n!./nls/columnHider", "xstyle/css!../css/extensions/ColumnHider.css"],
-function(declare, has, listen, miscUtil, put, i18n){
+define(["dojo/_base/declare", "dojo/has", "dojo/on", "../util/misc", "put-selector/put", "dojo/i18n!./nls/columnHider", "dojo/dom-attr", "xstyle/css!../css/extensions/ColumnHider.css"],
+function(declare, has, listen, miscUtil, put, i18n, domAttr){
 /*
  *	Column Hider plugin for dgrid
  *	Originally contributed by TRT 2011-09-28
@@ -29,7 +29,10 @@ function(declare, has, listen, miscUtil, put, i18n){
 		// Given one of the checkboxes from the hider menu,
 		// return the id of the corresponding column.
 		// (e.g. gridIDhere-hider-menu-check-colIDhere -> colIDhere)
-		return cb.id.substr(grid.id.length + 18);
+
+		//return cb.id.substr(grid.id.length + 18);
+		//MR: added to support nested properties
+		return domAttr.get(cb.id, "data-field");
 	}
 	
 	return declare(null, {
@@ -111,7 +114,7 @@ function(declare, has, listen, miscUtil, put, i18n){
 			// put-selector can't handle invalid selector characters, and the
 			// ID could have some, so add it directly
 			checkbox = this._columnHiderCheckboxes[id] =
-				put(div, "input.dgrid-hider-menu-check.hider-menu-check-" + replacedId + "[type=checkbox]");
+				put(div, "input.dgrid-hider-menu-check.hider-menu-check-" + replacedId + "[type=checkbox][data-field=" + id + "]"); //MR added data-field attribute for nested properties support
 			checkbox.id = checkId;
 			
 			label = put(div, "label.dgrid-hider-menu-label.hider-menu-label-" + replacedId +
